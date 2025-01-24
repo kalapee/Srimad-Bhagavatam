b1081408e320df1b60dc5b58cbab1a576db1cb4c
Kalapee Gajjar <35555664+kalapee@users.noreply.github.com>
2025-01-24 15:23:21 +0000

    Two Adhyay

---
+++ bhagavatam/src/app/Componet/Two.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Two() {
+  return (
+    <div>
+      <h1 className='text-center text-blue-400'>Two All</h1>
+    </div>
+  )
+}
+
+export default Two
--- bhagavatam/src/app/page.tsx
+++ bhagavatam/src/app/page.tsx
@@ -1,9 +1,16 @@
+
+
 import Image from "next/image";
+import Two from "./Componet/Two";
 
 export default function Home() {
   return (
-   <div className="text-center text-6xl text-blue-500">
-    Srimad-Bhagavatam
+  <> <div>
+    <p  className="text-center text-6xl text-blue-500">Srimad-Bhagavatam</p>
+    
+    
    </div>
+   <Two />
+   </>
   );
 }
